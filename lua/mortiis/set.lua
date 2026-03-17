0771ce1811a8ba7048903c30698f81fa9f0daed2
Benjamin Mudry <benjamin.mudry@terago.ca>
2026-03-17 14:43:41 -0400

    added fugitive to the status line

--- lua/mortiis/set.lua
+++ lua/mortiis/set.lua
@@ -40,6 +40,9 @@ vim.opt.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
+-- Show Fugitive Status Line
+vim.opt.statusline:append '%{FugitiveStatusline()}'
+
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
